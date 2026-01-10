e2a680d05e7e95a93a4fd16b2b78e421c697456f
Dmitriy Gaydukov <gaydukov89@gmail.com>
2026-01-10 18:58:14 +0400

    add working host version

--- host/src/main/java/com/java/host/HostRestApp.java
+++ host/src/main/java/com/java/host/HostRestApp.java
@@ -1,13 +1,11 @@
-package com.example.demo;
+package com.java.host;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
-
+public class HostRestApp {
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(HostRestApp.class, args);
 	}
-
 }
